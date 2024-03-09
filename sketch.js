42dce38ef62b715b96e3639815d82d724af8e48f
KiisiAde <152005440+KiisiAde@users.noreply.github.com>
2024-03-09 12:17:34 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var ball;
+var ground;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  ground1 = Bodies.rectangle(300,200,300,20,ground_options);
+  World.add(world,ground1);
+  rock=Bodies.circle(300,20,20,ball_options);
+   World.add(world,rock);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+  rect(ground.position.x,ground.position.y,400,20);
+  rect(ground1.position.x,ground1.position.y,200,20);
+  ellipse(rock.position.x,rock.position.y,20,20);
+ 
+}
+
